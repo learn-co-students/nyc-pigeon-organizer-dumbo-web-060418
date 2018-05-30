ec321f61518a95e57ef0d3063baf2edc98765559
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-30 01:58:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,13 @@
-require "pry"
-
 def nyc_pigeon_organizer(data)
-  new_ar = []
-  
-  new_hash = Hash.new{|hsh,key| hsh[key] = {} }
-#h['k1'].store 'a',1
-#h['k1'].store 'b',1
+ new_hash = Hash.new{|hsh,key| hsh[key] = {} }
   
   data.collect do |key_one, bulk_data|
     bulk_data.collect do |key_two, names|
       names.collect do |name|
+        
         if new_hash[name][key_one]
           new_hash[name][key_one] << key_two.to_s
+          
         else
           new_hash[name].store(key_one, [key_two.to_s])
         
@@ -20,27 +16,8 @@ def nyc_pigeon_organizer(data)
   end
   end
       new_hash
-      
-    end
+  end
 
 
 
 
-nyc_pigeon_organizer(pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-})
