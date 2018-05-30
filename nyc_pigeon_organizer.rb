5091da159ce8037990e00351701efcd8f2c47e1b
critsmet <36425627+critsmet@users.noreply.github.com>
2018-05-30 18:14:20 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+nameHash = {}
+data.each do |attribute, hash|
+  hash.each do |quality, names|
+    names.each do |name|
+    nameHash[name] ||= {}
+    nameHash[name][attribute] ||= []
+    nameHash[name][attribute] << quality.to_s
+      end
+    end
+  end
+   nameHash
 end
